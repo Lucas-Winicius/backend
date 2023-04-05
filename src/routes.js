bcac059d5e4e01ca1c71159514a6ea271ac08526
Lucas-Winicius <lucasclucas7@gmail.com>
2023-04-05 16:32:59 -0300

    Deletar

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,12 @@ const express = require("express");
 const Book = require("./models/Books");
 const routes = express();
 
-routes.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
-routes.post("/", (req, res) => {
-  res.send("Hello, " + req.query.name);
-});
+routes.delete("/books/:id", (req, res) => {
+  Book.findOneAndDelete({ id: req.params.id })
+    .then(deleted => res.status(200).json(deleted))
+    .catch(err => res.status(500).json(err))
+})
 
 routes.get("/books", (req, res) => {
   Book.find()
